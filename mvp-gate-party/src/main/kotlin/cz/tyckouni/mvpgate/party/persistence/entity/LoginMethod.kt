15a3699444c4a22f02920152d9cced5f3196c37a
Vojtech <vojtech.sassmann@gmail.com>
2023-04-04 20:37:17 +0200

    feature(party): added login method jpa

---
+++ mvp-gate-party/src/main/kotlin/cz/tyckouni/mvpgate/party/persistence/entity/LoginMethod.kt
@@ -0,0 +1,19 @@
+package cz.tyckouni.mvpgate.party.persistence.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+/**
+ * Login method JPA entity.
+ */
+@Entity(name = "login_method")
+class LoginMethod(
+    @ManyToOne
+    @JoinColumn(name = "sp_id", nullable = false)
+    var sp: Sp,
+
+    @ManyToOne
+    @JoinColumn(name = "idp_id", nullable = false)
+    var idp: Idp
+) : PartyEntity()
