8fa6edff9c6c0944efff15b3e61603efa8dfac04
Vojtech <vojtech.sassmann@gmail.com>
2023-04-26 21:27:59 +0200

    feat: distributed tracing
    * added basic support for distributed tracing to auth and party services

--- mvp-gate-auth/src/main/kotlin/cz/tyckouni/mvpgate/auth/controller/LoginController.kt
+++ mvp-gate-auth/src/main/kotlin/cz/tyckouni/mvpgate/auth/controller/LoginController.kt
@@ -1,21 +1,29 @@
 package cz.tyckouni.mvpgate.auth.controller
 
-import io.micrometer.tracing.Tracer
+import cz.tyckouni.mvpgate.grpc.ByGuidRequest
+import cz.tyckouni.mvpgate.party.grpc.IdpServiceGrpc
+import io.grpc.ManagedChannel
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 class LoginController(
-    val tracer: Tracer,
+    val partManagedChannel: ManagedChannel,
 ) {
 
     @GetMapping("/login/hello")
     fun hello(): String {
-        val span = tracer.currentSpan()
+        LOGGER.debug("Hello")
 
-        LOGGER.debug("Hello: {}", span)
+        IdpServiceGrpc.newBlockingStub(partManagedChannel)
+            .findByGuid(
+                ByGuidRequest.newBuilder()
+                    .setGuid(UUID.randomUUID().toString())
+                    .build(),
+            )
 
         return "hello"
     }
--- mvp-gate-party/src/main/kotlin/cz/tyckouni/mvpgate/party/grpc/IdpServiceGrpcImpl.kt
+++ mvp-gate-party/src/main/kotlin/cz/tyckouni/mvpgate/party/grpc/IdpServiceGrpcImpl.kt
@@ -3,7 +3,7 @@ package cz.tyckouni.mvpgate.party.grpc
 import cz.tyckouni.mvpgate.grpc.ByGuidRequest
 import cz.tyckouni.mvpgate.party.business.core.IdpService
 import cz.tyckouni.mvpgate.party.grpc.converter.IdpGrpcConverter
-import io.micrometer.tracing.Tracer
+import io.grpc.stub.StreamObserver
 import org.lognet.springboot.grpc.GRpcService
 import org.slf4j.LoggerFactory
 import java.util.UUID
@@ -15,11 +15,11 @@ import java.util.UUID
 class IdpServiceGrpcImpl(
     private val idpService: IdpService,
     private val converter: IdpGrpcConverter,
-    private val tracer: Tracer,
-) : IdpServiceGrpcKt.IdpServiceCoroutineImplBase() {
+) : IdpServiceGrpc.IdpServiceImplBase() {
 
-    override suspend fun findByGuid(request: ByGuidRequest): IdpGrpc {
-        val span = tracer.startScopedSpan("IDP.FIND_BY_GUID")
+    override fun findByGuid(request: ByGuidRequest?, responseObserver: StreamObserver<IdpGrpc>?) {
+        requireNotNull(request)
+        requireNotNull(responseObserver)
 
         LOGGER.debug("Call to IdpService.findByGuid: {}", request)
 
@@ -27,11 +27,9 @@ class IdpServiceGrpcImpl(
             .map(converter::toGrpc)
             .orElse(IdpGrpc.getDefaultInstance())
 
-        span.end()
-
-        return idp
+        responseObserver.onNext(idp)
+        responseObserver.onCompleted()
     }
-
     companion object {
         private val LOGGER = LoggerFactory.getLogger(IdpServiceGrpcImpl::class.java)
     }
