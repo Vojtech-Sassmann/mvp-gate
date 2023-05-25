ecc994d04b2f5ebc0fe6d224214f6738ceeec1ce
Vojtech <vojtech.sassmann@gmail.com>
2023-05-25 21:02:36 +0200

    feature(party): createIdp validations

---
+++ party/business/src/main/kotlin/cz/tyckouni/mvpgate/party/business/usecase/validation/ValidationError.kt
@@ -0,0 +1,8 @@
+package cz.tyckouni.mvpgate.party.business.usecase.validation
+
+/**
+ * Data class of a validation error
+ */
+data class ValidationError(
+    val message: String,
+)
--- party/business/src/test/kotlin/cz/tyckouni/mvpgate/party/business/usecase/create/idp/CreateIdpInteractorTest.kt
+++ party/business/src/test/kotlin/cz/tyckouni/mvpgate/party/business/usecase/create/idp/CreateIdpInteractorTest.kt
@@ -4,7 +4,9 @@ import cz.tyckouni.mvpgate.party.business.entity.CommonIdp
 import cz.tyckouni.mvpgate.party.business.entity.Idp
 import cz.tyckouni.mvpgate.party.business.gateway.GuidProvider
 import cz.tyckouni.mvpgate.party.business.gateway.Idps
+import cz.tyckouni.mvpgate.party.business.usecase.validation.ValidationException
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.kotlin.argumentCaptor
@@ -16,13 +18,13 @@ internal class CreateIdpInteractorTest {
 
     private val idps = Mockito.mock(Idps::class.java)
     private val idpArgumentCaptor = argumentCaptor<Idp>()
-    private val expectedIdp = CommonIdp("guid", "cool-idp", "https://login")
+    private val expectedIdp = CommonIdp("guid", "cool-idp", "http://localhost:8000")
     private val guidProvider = GuidProvider { expectedIdp.getGuid() }
 
     private val createIdpInteractor = CreateIdpInteractor(idps, guidProvider)
 
     @Test
-    fun `create assigns correct fields`() {
+    fun `assigns correct fields`() {
         Mockito.doNothing()
             .`when`(idps)
             .save(idpArgumentCaptor.capture())
@@ -34,5 +36,23 @@ internal class CreateIdpInteractorTest {
         assertThat(savedIdp)
             .usingRecursiveComparison()
             .isEqualTo(expectedIdp)
+    }
+
+    @Test
+    fun `fails for invalid login url`() {
+        val createIdpRequest = CreateIdpRequest(expectedIdp.getName(), "http://localhost:900 ")
+
+        assertThatExceptionOfType(ValidationException::class.java)
+            .isThrownBy { createIdpInteractor.create(createIdpRequest) }
+            .withMessageContaining("Validation failed: ['loginUrl is not a valid URL']")
+    }
+
+    @Test
+    fun `fails for empty name`() {
+        val createIdpRequest = CreateIdpRequest("", expectedIdp.getLoginUrl())
+
+        assertThatExceptionOfType(ValidationException::class.java)
+            .isThrownBy { createIdpInteractor.create(createIdpRequest) }
+            .withMessageContaining("Validation failed: ['name cannot be blank']")
     }
 }
