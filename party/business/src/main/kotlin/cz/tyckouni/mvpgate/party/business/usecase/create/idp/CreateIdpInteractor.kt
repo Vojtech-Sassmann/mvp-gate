8e5420d2335fd2e76e3726edb25a68b1c6ec4f29
Vojtech <vojtech.sassmann@gmail.com>
2023-05-25 21:02:36 +0200

    feature(party): createIdp validations

--- party/business/src/main/kotlin/cz/tyckouni/mvpgate/party/business/usecase/create/idp/CreateIdpInteractor.kt
+++ party/business/src/main/kotlin/cz/tyckouni/mvpgate/party/business/usecase/create/idp/CreateIdpInteractor.kt
@@ -4,13 +4,23 @@ import cz.tyckouni.mvpgate.party.business.entity.CommonIdp
 import cz.tyckouni.mvpgate.party.business.entity.Idp
 import cz.tyckouni.mvpgate.party.business.gateway.GuidProvider
 import cz.tyckouni.mvpgate.party.business.gateway.Idps
+import cz.tyckouni.mvpgate.party.business.usecase.validation.UrlValidator
+import cz.tyckouni.mvpgate.party.business.usecase.validation.Validator
 
+/**
+ * Interactor of the [CreateIdpUseCase]
+ */
 class CreateIdpInteractor(
     private val idps: Idps,
     private val guidProvider: GuidProvider,
 ) : CreateIdpUseCase {
 
     override fun create(createIdpRequest: CreateIdpRequest): Idp {
+        Validator()
+            .validate(UrlValidator.isValidUrl(createIdpRequest.loginUrl), "loginUrl is not a valid URL")
+            .validate(createIdpRequest.name.isNotBlank(), "name cannot be blank")
+            .handle()
+
         val newIdp = CommonIdp(guidProvider.newGuid(), createIdpRequest.name, createIdpRequest.loginUrl)
 
         idps.save(newIdp)
